e895a645eb125f265888d5554d05c8bdec06d1ea
nkosinathintuli <nkosinathintuli21@gmail.com>
2022-09-18 19:49:01 +0200

    all button functions implemented todo: overview button

--- app/src/main/java/com/example/alphabetbook/LetterPageActivity.kt
+++ app/src/main/java/com/example/alphabetbook/LetterPageActivity.kt
@@ -2,12 +2,81 @@ package com.example.alphabetbook
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.ImageView
+import com.example.alphabetbook.R.drawable
 
 class LetterPageActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_letter_page)
 
+        var name: String? = intent.getStringExtra("name")
+        var id: Int = resources.getIdentifier(name, "drawable", packageName)
+        var imageView = findViewById<ImageView>(R.id.imageView)
+        imageView.setImageResource(id)
 
+        val prevButton: Button = findViewById(R.id.button)
+        val aButton: Button = findViewById(R.id.button2)
+        val zButton: Button = findViewById(R.id.button3)
+        val nextButton: Button = findViewById(R.id.button4)
+
+        aButton.setOnClickListener {
+            id = resources.getIdentifier("slide01", "drawable", packageName)
+            imageView.setImageResource(id)
+        }
+        zButton.setOnClickListener {
+            id = resources.getIdentifier("slide26", "drawable", packageName)
+            imageView.setImageResource(id)
+        }
+        nextButton.setOnClickListener {
+            id = nextImage(id)
+            imageView.setImageResource(id)
+        }
+        prevButton.setOnClickListener {
+            id = prevImage(id)
+            imageView.setImageResource(id)
+        }
+    }
+
+    fun nextImage(id:Int): Int{
+        var filesNames = listOf("slide01", "slide02", "slide03", "slide04", "slide05", "slide06",
+            "slide07", "slide08","slide09","slide10","slide11","slide12","slide13","slide14","slide15",
+            "slide16","slide17","slide18","slide19","slide20","slide21","slide22","slide23","slide24",
+            "slide25","slide26")
+        println("next pressed")
+        println(id)
+        var idx = id
+        var i = 0
+        for(fName in filesNames){
+            if(fName == "slide26") break
+            if(id == resources.getIdentifier(fName, "drawable", packageName)){
+                i = filesNames.indexOf(fName)
+                idx = resources.getIdentifier(filesNames[i+1],"drawable", packageName)
+                break
+            }
+        }
+        return idx
+    }
+
+    fun prevImage(id: Int): Int{
+        var filesNames = listOf("slide01", "slide02", "slide03", "slide04", "slide05", "slide06",
+            "slide07", "slide08","slide09","slide10","slide11","slide12","slide13","slide14","slide15",
+            "slide16","slide17","slide18","slide19","slide20","slide21","slide22","slide23","slide24",
+            "slide25","slide26")
+        println("prev pressed")
+        println(id)
+        var idx = id
+        var i = 0
+        for(fName in filesNames){
+            if(fName == "slide01") continue
+            if(id == resources.getIdentifier(fName, "drawable", packageName)){
+                i = filesNames.indexOf(fName)
+                idx = resources.getIdentifier(filesNames[i-1],"drawable", packageName)
+                break
+            }
+        }
+        return idx
     }
 }
--- app/src/main/java/com/example/alphabetbook/MainActivity.kt
+++ app/src/main/java/com/example/alphabetbook/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.alphabetbook
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
@@ -10,10 +11,146 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val buttonA: Button = findViewById(R.id.buttonA)
-        buttonA.setOnClickListener {
-            var intent = Intent(this, LetterPageActivity::class.java)
-            startActivity(intent)
+        //val buttonA: Button = findViewById(R.id.buttonA)
+//        buttonA.setOnClickListener {
+//            var intent = Intent(this, LetterPageActivity::class.java)
+//            startActivity(intent)
+//        }
+    }
+
+    fun buttonClicked(view: View) {
+        when(view.getId()) {
+            R.id.buttonA -> {
+                var intent = Intent(this, LetterPageActivity::class.java)
+                intent.putExtra("name", "slide01" )
+                startActivity(intent)
+            }
+            R.id.buttonB -> {
+                var intent = Intent(this, LetterPageActivity::class.java)
+                intent.putExtra("name", "slide02" )
+                startActivity(intent)
+            }
+            R.id.buttonC -> {
+                var intent = Intent(this, LetterPageActivity::class.java)
+                intent.putExtra("name", "slide03" )
+                startActivity(intent)
+            }
+            R.id.buttonD -> {
+                var intent = Intent(this, LetterPageActivity::class.java)
+                intent.putExtra("name", "slide04" )
+                startActivity(intent)
+            }
+            R.id.buttonE -> {
+                var intent = Intent(this, LetterPageActivity::class.java)
+                intent.putExtra("name", "slide05" )
+                startActivity(intent)
+            }
+            R.id.buttonF -> {
+                var intent = Intent(this, LetterPageActivity::class.java)
+                intent.putExtra("name", "slide06" )
+                startActivity(intent)
+            }
+            R.id.buttonG -> {
+                var intent = Intent(this, LetterPageActivity::class.java)
+                intent.putExtra("name", "slide07" )
+                startActivity(intent)
+            }
+            R.id.buttonH -> {
+                var intent = Intent(this, LetterPageActivity::class.java)
+                intent.putExtra("name", "slide08" )
+                startActivity(intent)
+            }
+            R.id.buttonI -> {
+                var intent = Intent(this, LetterPageActivity::class.java)
+                intent.putExtra("name", "slide09" )
+                startActivity(intent)
+            }
+            R.id.buttonJ -> {
+                var intent = Intent(this, LetterPageActivity::class.java)
+                intent.putExtra("name", "slide10" )
+                startActivity(intent)
+            }
+            R.id.buttonK -> {
+                var intent = Intent(this, LetterPageActivity::class.java)
+                intent.putExtra("name", "slide11" )
+                startActivity(intent)
+            }
+            R.id.buttonL -> {
+                var intent = Intent(this, LetterPageActivity::class.java)
+                intent.putExtra("name", "slide12" )
+                startActivity(intent)
+            }
+            R.id.buttonM -> {
+                var intent = Intent(this, LetterPageActivity::class.java)
+                intent.putExtra("name", "slide13" )
+                startActivity(intent)
+            }
+            R.id.buttonN -> {
+                var intent = Intent(this, LetterPageActivity::class.java)
+                intent.putExtra("name", "slide14" )
+                startActivity(intent)
+            }
+            R.id.buttonO -> {
+                var intent = Intent(this, LetterPageActivity::class.java)
+                intent.putExtra("name", "slide15" )
+                startActivity(intent)
+            }
+            R.id.buttonP -> {
+                var intent = Intent(this, LetterPageActivity::class.java)
+                intent.putExtra("name", "slide16" )
+                startActivity(intent)
+            }
+                R.id.buttonQ -> {
+                var intent = Intent(this, LetterPageActivity::class.java)
+                intent.putExtra("name", "slide17" )
+                startActivity(intent)
+            }
+            R.id.buttonR -> {
+                var intent = Intent(this, LetterPageActivity::class.java)
+                intent.putExtra("name", "slide18" )
+                startActivity(intent)
+            }
+            R.id.buttonS -> {
+                var intent = Intent(this, LetterPageActivity::class.java)
+                intent.putExtra("name", "slide19" )
+                startActivity(intent)
+            }
+            R.id.buttonT -> {
+                var intent = Intent(this, LetterPageActivity::class.java)
+                intent.putExtra("name", "slide20" )
+                startActivity(intent)
+            }
+            R.id.buttonU -> {
+                var intent = Intent(this, LetterPageActivity::class.java)
+                intent.putExtra("name", "slide21" )
+                startActivity(intent)
+            }
+            R.id.buttonV -> {
+                var intent = Intent(this, LetterPageActivity::class.java)
+                intent.putExtra("name", "slide22" )
+                startActivity(intent)
+            }
+            R.id.buttonW -> {
+                var intent = Intent(this, LetterPageActivity::class.java)
+                intent.putExtra("name", "slide23" )
+                startActivity(intent)
+            }
+            R.id.buttonX -> {
+                var intent = Intent(this, LetterPageActivity::class.java)
+                intent.putExtra("name", "slide24" )
+                startActivity(intent)
+            }
+            R.id.buttonY -> {
+                var intent = Intent(this, LetterPageActivity::class.java)
+                intent.putExtra("name", "slide25" )
+                startActivity(intent)
+            }
+            R.id.buttonZ -> {
+                var intent = Intent(this, LetterPageActivity::class.java)
+                intent.putExtra("name", "slide26" )
+                startActivity(intent)
+            }
+
         }
     }
 }
